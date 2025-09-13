3a3f103aae53c3e81894c4fc52836f3187a55907
Prabhjotbh <prabhjotbhatia09@gmail.com>
2025-09-14 00:46:38 +0530

    changes

--- ledger/include/Utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <llvm/IR/Module.h>
-
-size_t countInstructions(llvm::Module &M);
-size_t countFunctions(llvm::Module &M);
-size_t countBasicBlocks(llvm::Module &M);
-size_t estimateIRSize(llvm::Module &M);
-void writeModuleToFile(llvm::Module &M, const std::string &path);
